123b7db04ef283d0ccf6919cca916ab7caae4cc3
oceanshowers3 <73365515+oceanshowers3@users.noreply.github.com>
2021-05-24 11:56:59 -0400

    selected colors for mag

--- LeafLet/static/js/plot.js
+++ LeafLet/static/js/plot.js
@@ -22,3 +22,61 @@ streetmap.addTo(myMap);
  // API endpoint
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
+// Fetch earthquake json
+d3.json(queryUrl, function(data) {
+
+    // style map
+    function mapStyle(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: mapColor(feature.properties.mag),
+            color: "#000000",
+            radius: mapRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+
+    // colors for magnitude sizes
+    function mapColor(mag) {
+        switch (true) {
+            case mag > 5:
+            return "#db170d";
+            case mag > 4:
+            return "#ff3300";
+            case mag > 3:
+            return "#ff9900";
+            case mag > 2:
+            return "#8be109";
+            case mag > 1:
+            return "#00e600";
+            default:
+            return "#00cccc";
+        }
+    }
+    // radiuses for magnitude sizes
+    function mapRadius(mag) {
+      if (mag === 0) {
+        return 1;
+      }
+  
+      return mag * 4;
+    }
+    
+  
+  
+    L.geoJson(data, {
+  
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+  
+      style: mapStyle,
+  
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  
+      }
+    }).addTo(myMap);
+  
